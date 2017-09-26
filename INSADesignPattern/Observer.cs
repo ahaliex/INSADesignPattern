dff0a09021e3c8ae0382f0aa286a2921f9414067
ahaliex <alexandre.muller@outlook.fr>
2017-09-26 16:36:26 +0200

    TD1

---
+++ INSADesignPattern/Observer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSADesignPattern
+{
+    class Observer
+    {
+        private Dictionary<string, List<IObservable>> listeners;
+
+        public Observer()
+        {
+            listeners = new Dictionary<string, List<IObservable>>();
+        }
+
+        //Register a listener for an input
+        public void Register(string input, IObservable observable)
+        {
+            //If the listener list does contain the input
+            if (!listeners.ContainsKey(input))
+                //add a list for that input
+                listeners.Add(input, new List<IObservable>());
+
+            listeners.Single(p => p.Key == input).Value.Add(observable);
+        }
+
+
+        //Unregister a listener
+        public void Unregister(string input, IObservable observable)
+        {
+            if (!listeners.ContainsKey(input))
+                return;
+
+            listeners.Single(p => p.Key == input).Value.Remove(observable);
+        }
+
+        public bool Trigger(string input)
+        {
+            List<IObservable> inputListeners = listeners.SingleOrDefault(p => p.Key == input).Value;
+
+            if (inputListeners == null || inputListeners.Count == 0)
+                return false;
+
+            foreach (IObservable observable in inputListeners)
+            {
+                if (!observable.Execute())
+                    return true;
+            }
+
+            return true;
+        }
+    }
+}
--- INSADesignPattern/Program.cs
+++ INSADesignPattern/Program.cs
@@ -18,7 +18,7 @@ namespace INSADesignPattern
             Console.WriteLine("  /  / /    |/  / /____   / /  ___  |");
             Console.WriteLine(" /  / /  /|    / _____/  / /  /   | |");
             Console.WriteLine("/__/ /__/ |___/ /_______/ /__/    |_|");
-            Console.WriteLine("Desing Patterns - Anthony Maudry amaudry@gmail.com");
+            Console.WriteLine("Design Patterns - Anthony Maudry amaudry@gmail.com");
             Console.WriteLine("Hello,");
             Console.WriteLine("Write something (type 'exit' to exit the program).");
             while ((line = Console.ReadLine()) != "exit")
